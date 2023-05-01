fb2e7e5919eeece5f7724a3aa67f5a62a349ae7b
kaiya <kaiya.xiong337@imaygou.com>
2023-05-01 19:49:39 +0800

    update

--- src/server.rs
+++ src/server.rs
@@ -14,14 +14,15 @@ impl Greeter for MyGreeter {
     async fn say_hello(
         &self,
         request: Request<HelloRequest>, // Accept request of type HelloRequest
-    ) -> Result<Response<HelloReply>, Status> { // Return an instance of type HelloReply
+    ) -> Result<Response<HelloReply>, Status> {
+        // Return an instance of type HelloReply
         println!("Got a request: {:?}", request);
 
         let reply = hello_world::HelloReply {
-            message: format!("Hello {}!", request.into_inner().name).into(), // We must use .into_inner() as the fields of gRPC requests and responses are private
+            message: format!("Hello {}!", request.into_inner().name), // We must use .into_inner() as the fields of gRPC requests and responses are private
         };
 
-        Ok(Response::new(reply)) // Send back our formatted greeting
+        Ok(Response::new(reply))
     }
 }
 #[tokio::main]
